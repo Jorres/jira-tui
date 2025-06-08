e821ab3e0b0c4d00519b82f42e4dd26c50338224
Jorres Tarasov <jorres.tarasov@gmail.com>
2025-06-08 19:34:04 +0200

    fix: remove corrupting markdown

--- pkg/md/md.go
+++ pkg/md/md.go
@@ -1,22 +1,12 @@
 package md
 
 import (
-	cf "github.com/kentaro-m/blackfriday-confluence"
-	bf "github.com/russross/blackfriday/v2"
-
 	"github.com/jorres/jira-tui/pkg/md/jirawiki"
 )
 
 // ToJiraMD translates CommonMark to Jira flavored markdown.
 func ToJiraMD(md string) string {
-	if md == "" {
-		return md
-	}
-
-	renderer := &cf.Renderer{Flags: cf.IgnoreMacroEscaping}
-	r := bf.New(bf.WithRenderer(renderer), bf.WithExtensions(bf.CommonExtensions))
-
-	return string(renderer.Render(r.Parse([]byte(md))))
+	return md
 }
 
 // FromJiraMD translates Jira flavored markdown to CommonMark.
